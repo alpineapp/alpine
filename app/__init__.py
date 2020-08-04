33690632540c8020274050c556bbb7e800fc471a
Dinh Van Quy <quy.dinh3195@gmail.com>
2020-08-04 07:02:12 +0700

    Deploy app with Heroku Git
    I preferred to deploy with Docker but failed.
    I switch to this as both Heroku and Miguel Flask Tutorial better support this type of deployment.
    
    Resolves: #6

--- app/__init__.py
+++ app/__init__.py
@@ -65,7 +65,7 @@ def create_app(config_class=Config):
         else:
             if not os.path.exists('logs'):
                 os.mkdir('logs')
-            file_handler = RotatingFileHandler('logs/microblog.log',
+            file_handler = RotatingFileHandler('logs/alpine.log',
                                             maxBytes=10240, backupCount=10)
             file_handler.setFormatter(logging.Formatter(
                 '%(asctime)s %(levelname)s: %(message)s '
