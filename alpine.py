58a6fd98067832d9dfbd2ce6765873694ef4f26b
Binh Do <binhdoq@gmail.com>
2021-01-21 22:39:10 +0700

    DEV-36 Remove current user from alpine.py

--- alpine.py
+++ alpine.py
@@ -2,18 +2,16 @@ import os
 
 from app import create_app, db
 from app.models import User, Card, Notification, Task, Deck
-from flask_login import login_user, current_user
+from flask_login import login_user
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    user = User.query.filter_by(username='quy').first()
     request_ctx = app.test_request_context()
     request_ctx.push()
-    login_user(user)
     return {'db': db, 'User': User, 'Card': Card, 'Notification': Notification,
-            'Deck': Deck, 'Task': Task, 'current_user': user}
+            'Deck': Deck, 'Task': Task}
 
 @app.template_filter()
 def get_env(key):
